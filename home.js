fe8bbb9ab65f8a0e7ee325bc550d0261759bff1f
Awdhesh Kumar <awdheshkumarab98@gmail.com>
2023-01-28 14:44:59 +0530

    All files are added

---
+++ home.js
@@ -0,0 +1,15 @@
+var obj = [{
+      id:1,
+      fname:"Awdhesh",
+      lname:"kumar",
+      contact:"964513465"
+}]
+
+function submit(){
+  var fname= document.getElementById('name').value;
+  var lname= document.getElementById('lname').value;
+  var contact= document.getElementById('contact').value;
+  console.log(fname);
+  console.log(lname);
+  console.log(contact);
+}
